c7e61e75da031b9bb6b21c977b08bd582d2a05e4
GLEDSONS831 <glegogle84@gmail.com>
2020-05-10 22:07:50 -0300

    :pencil: add javascript in html

--- database/main.js
+++ database/main.js
@@ -1,7 +1,62 @@
-const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
+var index = 0;
+const state = {
+    photos : [],
+    props : [],
+    names : [],
+    prices : [],
+    page : 0
+}
 
-fetch(url).then((data) =>{
-    return data.json();
-}).then((i) =>{
-    console.log(i)
-})
+function setState(state, list, page){
+    clearState(state)
+    limit = 6;
+    if(page){
+        var limit = (page+1) * 6;
+    }
+    index_aux = 0;
+    for(i = limit-6; i < limit; i++){
+        state.photos.push(list[index_aux].photo);
+        state.props.push(list[index_aux].property_type);
+        state.names.push(list[index_aux].name);
+        state.prices.push(list[index_aux++].price);
+        state.page = page;
+    }
+    console.log(state)
+}
+
+function clearState(state){
+    for(i = 0; i < 6; i++){
+        state.photos = []
+        state.props = []
+        state.names = []
+        state.prices = []
+        state.page = 0;
+    }
+}
+
+function showPhotos(index, list){
+    limit = 6;
+    if(index){
+        var limit = (index+1) * 6;
+    }
+    
+    for(i = limit-6; i < limit; i++){
+        console.log(list[i].photo)
+        console.log(i)
+    }
+}
+
+async function getData(index){
+    const data = await fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72');
+    data.json().then(
+        (data) =>{
+            var places = [...data]
+            
+            setState(state, places, index);
+            //showPhotos(index, places);
+            
+        }
+    )
+}
+
+export default state;
